44593b793e87b1d242707200534b6cb7f3c6dede
andrehenrique1986 <andrehenriquelima1986@gmail.com>
2023-10-15 01:12:54 -0300

    Listar Cursos (API HTTP GET)

--- CrudSpringApplication.java
+++ CrudSpringApplication.java
@@ -1,7 +1,12 @@
 package com.andre.crudspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.andre.crudspring.model.Course;
+import com.andre.crudspring.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -10,4 +15,20 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
+    /**
+     * @param courseRepository
+     * @return
+     */
+    @Bean	
+    CommandLineRunner initDatabase(CourseRepository courseRepository){
+      return args -> {
+		courseRepository.findAll();
+
+		Course c = new Course();
+		c.setName("Angular com Spring");		
+        c.setCategory("Front-End");
+
+		courseRepository.save(c);
+	  };
+    }	
 }
---
+++ controller/CourseController.java
@@ -0,0 +1,27 @@
+package com.andre.crudspring.controller;
+
+import com.andre.crudspring.model.Course;
+import com.andre.crudspring.repository.CourseRepository;
+
+
+import lombok.AllArgsConstructor;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {    
+    
+
+    private final CourseRepository courseRepository;
+
+
+    @GetMapping    
+    public List<Course> list(){
+        return courseRepository.findAll();
+    }
+
+}    
