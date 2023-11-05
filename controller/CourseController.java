634061b378c5229dbe113f6cafe1ce58ef80b301
andrehenrique1986 <andrehenriquelima1986@gmail.com>
2023-11-05 00:17:40 -0300

    API Spring: Criar Curso (HTTP POST)

--- controller/CourseController.java
+++ controller/CourseController.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,7 +21,7 @@ public class CourseController {
 
 
     @GetMapping    
-    public List<Course> list(){
+    public @ResponseBody List<Course> list(){
         return courseRepository.findAll();
     }
 
